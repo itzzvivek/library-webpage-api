8b7f17ca2433b3e358d58289fcac869e2f184375
vivek <vivekwarkade000@gmail.com>
2023-02-23 16:39:35 +0530

    initial commit

---
+++ library_project/core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Books
+# Create your views here.
+
+class BookListView(ListView):
+    model = Books 
+    template_name = 'core/book_list.html'
